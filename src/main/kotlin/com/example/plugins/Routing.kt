d418abb18399e7ceae42eaf836268a6f63ff0548
Rocco Schulz <rocco@is-gr8.com>
2021-09-30 21:04:10 +0200

    cleanup imports

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,11 +1,9 @@
 package com.example.plugins
 
 import com.example.TimeInfo
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
 
 fun Application.configureRouting() {
 
